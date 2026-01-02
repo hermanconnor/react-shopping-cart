149d06d4e5a43fa2ba80a0b702b956a019755771
herman <hermanconnor11@gmail.com>
2026-01-01 19:04:01 -0500

    Create proxy for server in vite config and create api-service

---
+++ src/services/api-service.ts
@@ -0,0 +1,11 @@
+import type { Product } from '@/types';
+
+export const getProducts = async (): Promise<Product[]> => {
+  const response = await fetch('api/products');
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch products');
+  }
+
+  return await response.json();
+};
